a99889cb21d9a1bcbbe92cde613d299bfc237476
raphael-khan <raphaelkhan@yahoo.com>
2022-06-28 21:22:49 -0400

    big o eval

---
+++ Algorithms/Big-O/big_o.js
@@ -0,0 +1,14 @@
+const nemo = ["nemo"]
+
+function find_nemo(array) {
+  let t0 = performance.now()
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === "nemo") {
+      console.log("Found Nemo !")
+    }
+  }
+  let t1 = performance.now()
+  console.log("call to find nemo took" + (t1 - t0) + "milliseconds")
+}
+
+find_nemo(nemo)
