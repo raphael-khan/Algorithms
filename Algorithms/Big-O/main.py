39667ceb1b4c3cad2cbb74cd343aa9e873280651
raphael-khan <raphaelkhan@yahoo.com>
2022-07-24 21:31:39 -0400

    arrays practice

--- Algorithms/Big-O/main.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def running_sum(self, nums):
-        for i in range(len(nums)):
-            sum(nums[i+1])
-        return nums
-
-running_sum()
-
--- Algorithms/arrays101/consecutiveOnes.py
+++ Algorithms/arrays101/consecutiveOnes.py
@@ -1,6 +1,6 @@
 # Given a binary array nums, return the maximum number of consecutive 1's in the array.
 # You see two sequences of 1's. The first two digits and the last three. You need to return 3 as that is the max number of seq 1
-arr = [1,1,0,1,1,1]
+nums = [0,0,0,1,1]
 
 def maxConsecutiveOnes(arr, n):
     count = 0
@@ -13,7 +13,9 @@ def maxConsecutiveOnes(arr, n):
             result = max(result, count)
     return result
 
-n = len(arr)
-print(maxConsecutiveOnes(arr, n))
+n = len(nums)
+print(maxConsecutiveOnes(nums, n))
+
+
 
 
