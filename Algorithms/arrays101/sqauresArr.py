ee3ba55389603e9814c2c2b22d5d0c892d509105
raphael-khan <raphaelkhan@yahoo.com>
2022-07-25 13:19:41 -0400

    two pointer approach

--- Algorithms/arrays101/sqauresArr.py
+++ Algorithms/arrays101/sqauresArr.py
@@ -7,11 +7,25 @@ nums = [-4,-1,0,3,10]
 # to iterate the array and square value at each index, map func
 # sort the result arr in asc order. 
 
-# using sort algorithm - quick sort.
+# using sort algorithm - quick sort= nlogn
 def sortedSquares():
     for i in range(len(nums)):
         nums[i] = nums[i]**2
     nums.sort()
     return nums
+
+# improving the time complex by not using the sort algo. O(n)
+
+def sortedSquares():
+    res = []
+    l, r = 0, len(nums)-1
+    while l <= r:
+        if nums[l]**2 > nums[r]**2:
+            res.append(nums[l]**2)
+            l += 1
+        else:
+            res.append(nums[r]**2)
+            r -= 1
+    return res[::-1] #reverse.
 
 print(sortedSquares())
