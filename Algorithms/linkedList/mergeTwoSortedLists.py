072b669a6a371e7d68a16a6c129cb4b552ffb37d
raphael-khan <raphaelkhan@yahoo.com>
2022-08-14 13:55:39 -0400

    updates

--- Algorithms/linkedList/mergeTwoSortedLists.py
+++ Algorithms/linkedList/mergeTwoSortedLists.py
@@ -17,21 +17,22 @@ class ListNode:
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         dummy = ListNode()
-        tail = dummy 
+        curr = dummy 
 
         while l1 and l2:
             if l1.val < l2.val:
-                tail.next = l1
+                curr.next = l1
                 l1 = l1.next
             else:
-                tail.next = l2
+                curr.next = l2
                 l2 = l2.next
-            tail = tail.next 
+
+            curr = curr.next 
 
         if l1:
-            tail.next = l1
+            curr.next = l1
         elif l2:
-            tail.next = l2
+            curr.next = l2
 
         return dummy.next
         
