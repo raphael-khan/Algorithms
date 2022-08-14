1639ac1c41d21a104601cb5fe133f65f2446984c
raphael-khan <raphaelkhan@yahoo.com>
2022-08-14 12:33:28 -0400

    reverseLinkedList

--- Algorithms/linkedList/reverseLinkedlist.py
+++ Algorithms/linkedList/reverseLinkedlist.py
@@ -1,5 +1,21 @@
 # Given the head of a singly linked list, reverse the list, and return the reversed list.
 
-# work in progress
-# hello
-# hello world
+Input = [1,2,3,4,5]
+Output = [5,4,3,2,1]
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def reverseList(self, head):
+        prev, curr = None, head
+        while curr:
+            tmp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = tmp
+        return prev
+
+print(Solution.reverseList(input))
