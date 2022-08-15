4ab276ac0aa3bcf8f2530e1c59b4babf986f0827
raphael-khan <raphaelkhan@yahoo.com>
2022-08-15 19:09:10 -0400

    updates to mergetwo lists

--- Algorithms/linkedList/mergeTwoSortedLists.py
+++ Algorithms/linkedList/mergeTwoSortedLists.py
@@ -15,24 +15,25 @@ class ListNode:
         self.val = val
         self.next = next
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode()
+    def mergeTwoLists(self, l1:ListNode, l2:ListNode) -> ListNode:
+        dummy = ListNode() # create a dummy node and add all nodes to it to handle edge case. 
         curr = dummy 
 
-        while l1 and l2:
-            if l1.val < l2.val:
+        while l1 and l2:   # when both are non empty / non null
+            if l1.val < l2.val: # if l1 value is less than l2, then add that l1 node to curr.
                 curr.next = l1
-                l1 = l1.next
-            else:
+                l1 = l1.next     # update l1 pointer.
+            else:                # if l2 is less or equal then add l2 node to curr
                 curr.next = l2
-                l2 = l2.next
+                l2 = l2.next     # update l2 pointer. 
 
-            curr = curr.next 
+            curr = curr.next     # update curr pointer regardless of whatever node is added. 
 
-        if l1:
+        if l1:                   # edge case if one list is empty. if l1 is not null, if l2 is not null. 
             curr.next = l1
         elif l2:
             curr.next = l2
 
         return dummy.next
+
         
