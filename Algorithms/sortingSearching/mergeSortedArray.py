20643f8996615624e7abe9cd1e629acc6ace1233
raphael-khan <raphaelkhan@yahoo.com>
2022-08-20 14:58:14 -0400

    mergeSortedArray

--- Algorithms/sortingSearching/mergeSortedArray.py
+++ Algorithms/sortingSearching/mergeSortedArray.py
@@ -12,19 +12,22 @@ Output = [1,2,2,3,5,6]
 
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        i,j = 0, 0
-        while i < len(nums1) and j < len(nums2):
-            if nums1[i] < nums2[j]:
-                i += 1 
-            elif nums1[i] == nums2[j]:
-                nums1[i + 1] = nums2[j]
-                i += 1
-                j =+ 1
-            elif nums2[j] > nums1[i]:
-                nums1[i + 1] == nums2[j]
-                i += 1
-                j += 1
-            else: 
-                nums1[ i - 1] == nums2[j]
-        return nums1
+        #last index of nums1
+        last = m + n - 1
+
+        # merge in reverse order
+        while m > 0 and n > 0: 
+            if nums1[m - 1] > nums2[n - 1]: # checking for the highest value and whatever is highest should be at nums1[last]
+                nums1[last] = nums1[m - 1]
+                m -= 1
+            else:  # else condition would be nums2 is > or = 
+                nums1[last] = nums2[n - 1]
+                n -= 1
+            last -= 1 # every time you append you will have to decrease last by 1 
+
+        # fill nums1 with leftover nums2 elements
+        while n > 0:
+            nums1[last] = nums2[n-1]
+            n, last = n - 1, last - 1
+
 
